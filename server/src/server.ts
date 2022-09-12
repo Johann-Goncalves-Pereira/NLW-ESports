df86ac5da545e304345d18077a89064adcacc336
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-09-12 15:04:52 -0300

    feat: server

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (req, res) => {
+  return res.json([
+    { id: 1, title: "Ad 1" },
+    { id: 2, title: "Ad 2" },
+    { id: 3, title: "Ad 3" },
+    { id: 3, title: "Ad 3" },
+    { id: 3, title: "Ad 3" },
+    { id: 3, title: "Ad 3" },
+    { id: 3, title: "Ad 3" },
+  ]);
+});
+
+app.listen(3333);
