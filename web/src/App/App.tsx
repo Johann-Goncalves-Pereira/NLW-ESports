f1123ff927696520cd01016b32d4b5a92397209a
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-09-13 14:31:49 -0300

    feat: styles

--- web/src/App/App.tsx
+++ web/src/App/App.tsx
@@ -1,6 +1,14 @@
 import Picture from "./Components/Picture/Picture";
 import logo from "/logo.svg";
 
+import styles from "./App.module.scss";
+
+type GameCardProps = {
+  src: string;
+  alt: string;
+  ulr: string;
+};
+
 const P = () => {
   return (
     <Picture
@@ -13,21 +21,48 @@ const P = () => {
   );
 };
 
+const GameCard = (p: GameCardProps) => {
+  return (
+    <li className={styles.card}>
+      <a href={p.ulr}>
+        <img
+          className="w-52 aspect-portrait object-cover"
+          src={p.src}
+          alt={p.alt}
+        />
+
+        <p>
+          <strong className="block font-600">League of Legends</strong>
+          <em className="text-zinc-300 text-sm">4 anúncios</em>
+        </p>
+      </a>
+    </li>
+  );
+};
+
 function App() {
   return (
     <>
       <P />
 
-      <div className="grid place-items-center gap-8 w-flued-1 my-4 mx-auto">
+      <div className="grid place-items-center gap-10 w-flued-1 py-8 mx-auto">
         <img className="" src={logo} alt="logo" />
 
         <h1 className="text-6xl text-surface-900 font-700">
           Seu
-          <span className="bg-clip-text text-transparent bg-gradient-100 ">
+          <span className="bg-clip-text text-transparent bg-gradient-text ">
             duo
           </span>
           está aqui.
         </h1>
+
+        <ul className={styles.cards}>
+          <GameCard ulr="#" src="https://picsum.photos/100/300" alt="picture" />
+          <GameCard ulr="#" src="https://picsum.photos/200/600" alt="picture" />
+          <GameCard ulr="#" src="https://picsum.photos/400/900" alt="picture" />
+          <GameCard ulr="#" src="https://picsum.photos/800/200" alt="picture" />
+          <GameCard ulr="#" src="https://picsum.photos/200/100" alt="picture" />
+        </ul>
       </div>
     </>
   );
